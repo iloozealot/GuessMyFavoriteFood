95e24c7cd1523adcefab4c9980dfbb8f195b9580
iloozealot <iloozealot@gmail.com>
2023-11-10 12:33:58 -0600

    Add project files.

---
+++ Welcometocoding/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2023 Guess my favorite food
+// Author: Justin Spencer
+// This program has the user type guesses until the correct guess is made.
+
+
+string? food;
+do
+{
+    Console.Write("Try to guess my favorite food: ");
+    food = Console.ReadLine();
+    if (food != "pizza")
+        Console.WriteLine("nope.");
+} while (food != "pizza");
+Console.WriteLine("Good job! Pizza is my favorite food.");
